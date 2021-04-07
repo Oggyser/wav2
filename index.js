4e92c4f4b3a67b15dfcd3da8a17d249f6d96ca97
Oggyser <81746672+Oggyser@users.noreply.github.com>
2021-04-07 19:00:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('JOCKER BOT ', {
+CFonts.say('OGGY SER', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
