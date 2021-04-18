616c207b57efbdc8ae138901346aa1e9c7e52b63
Oggyser <81746672+Oggyser@users.noreply.github.com>
2021-04-18 01:42:19 -0400

    Update oggyser.js

--- oggyser.js
+++ oggyser.js
@@ -5607,7 +5607,7 @@ case 'husbu':
         }
         if (budy.includes("don")){
 		denz.updatePresence(from, Presence.composing)
-		const loli = fs.readFileSync('./mp3/don.mp3')
+		const loli = fs.readFileSync('./mp3/oggydon.mp3')
         denz.sendMessage(from, loli, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
         const d = fs.readFileSync('./sticker/jget.webp');
         denz.sendMessage(from, d, sticker, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "song : dj tapi boong", 'jpegThumbnail': fs.readFileSync('./sticker/dnsnew.webp')}}}})
